6cb65d476895e6c4d3a1cceb416c3bdcf4848e59
YOUR NAME <YOUR@EMAIL.com>
2023-09-21 21:40:59 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+ 
+function destructivelyAppendCat(name) {
+  cats.push(name)
+}
+
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name)
+}
+
+
+function destructivelyRemoveLastCat() {
+  cats.pop()
+}
+
+
+function destructivelyRemoveFirstCat() {
+  cats.shift()
+}
+
+
+function appendCat(name) {
+  const Arnold = [...cats, name];
+  return Arnold
+}
+
+
+function prependCat(name) {
+  const Ralph = [name, ...cats];
+  return Ralph
+}
+
+function removeLastCat() {
+  const Melo = cats.slice(0, -1);
+  return Melo
+}
+
+function removeFirstCat() {
+  const kenn = cats.slice(0, -1);
+  return kenn
+}
+function removeFirstCat() {
+    const Melo = cats.slice(1);
+    return Melo
+  }
